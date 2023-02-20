d9157011d05fb2ae57155a0ca5cf3bb6af9e93ed
Niels Greven <avuc.site@gmail.com>
2023-02-20 12:18:10 +0100

    Added empty struct

---
+++ model/api/empty.go
@@ -0,0 +1,7 @@
+package api
+
+// Empty
+// An empty struct, needed for doing POST calls where no body is needed. Again, this is a workaround for the current
+// HTTP client.
+type Empty struct {
+}
