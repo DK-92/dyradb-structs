4ce492fab47ca2daaf70322e1b6495adae8363d1
Niels Greven <avuc.site@gmail.com>
2023-06-17 20:12:48 +0200

    Added optional labelcode to album

--- model/api/album.go
+++ model/api/album.go
@@ -3,18 +3,20 @@ package api
 // Album
 // Describes an album.
 type Album struct {
-	ID      string `json:"id,omitempty"`
-	Barcode string `json:"barcode" binding:"required,min=11,max=13,numeric"`
-	Artist  string `json:"artist" binding:"required,min=1,max=64"`
-	Album   string `json:"album" binding:"required,min=1,max=64"`
-	Label   string `json:"label,omitempty" binding:"omitempty,min=1,max=64"`
-	Country string `json:"country,omitempty" binding:"omitempty,len=2"`
-	Source  string `json:"source" binding:"required,oneof=CDROM SHMCD SACD DVDA BLURAY DOWNLOAD"`
-	Comment string `json:"comment,omitempty" binding:"omitempty,min=1,max=1024"`
+	ID            string `json:"id,omitempty"`
+	Barcode       string `json:"barcode" binding:"required,min=11,max=13,numeric"`
+	Artist        string `json:"artist" binding:"required,min=1,max=64"`
+	Album         string `json:"album" binding:"required,min=1,max=64"`
+	Label         string `json:"label,omitempty" binding:"omitempty,min=1,max=64"`
+	Country       string `json:"country,omitempty" binding:"omitempty,len=2"`
+	Source        string `json:"source" binding:"required,oneof=CDROM SHMCD SACD DVDA BLURAY DOWNLOAD"`
+	CatalogNumber string `json:"catalogNumber" binding:"omitempty,min=1,max=16"`
+	Comment       string `json:"comment,omitempty" binding:"omitempty,min=1,max=1024"`
 
 	DRLog string `json:"drLog,omitempty" binding:"required,drlog"`
 	Art   string `json:"art,omitempty" binding:"required,encodedimage"`
 
+	LabelCode    int  `json:"labelCode" binding:"omitempty,min=4,max=5"`
 	ReleaseYear  int  `json:"releaseYear" binding:"required,numeric,min=1000,max=3000"`
 	AverageDR    int8 `json:"averageDR"`
 	MinimumDR    int8 `json:"minimumDR"`
--- model/form/album.go
+++ model/form/album.go
@@ -3,16 +3,19 @@ package form
 // Album
 // Validates the add album form.
 type Album struct {
-	Artist      string `form:"artist" binding:"required,min=1,max=64"`
-	Album       string `form:"album" binding:"required,min=1,max=64"`
-	Barcode     string `form:"barcode" binding:"required" binding:"required,min=11,max=13,numeric"`
-	Source      string `form:"source" binding:"required,oneof=CDROM SHMCD SACD DVDA BLURAY DOWNLOAD"`
-	ReleaseYear int    `form:"release_year" binding:"required,numeric,min=1000,max=3000"`
+	Artist  string `form:"artist" binding:"required,min=1,max=64"`
+	Album   string `form:"album" binding:"required,min=1,max=64"`
+	Barcode string `form:"barcode" binding:"required" binding:"required,min=11,max=13,numeric"`
+	Source  string `form:"source" binding:"required,oneof=CDROM SHMCD SACD DVDA BLURAY DOWNLOAD"`
+
+	ReleaseYear int `form:"release_year" binding:"required,numeric,min=1000,max=3000"`
 
 	DRLog string
 	Art   string
 
-	Label   string `form:"label" binding:"omitempty,min=1,max=64"`
-	Country string `form:"country" binding:"omitempty,len=2"`
-	Comment string `form:"comment" binding:"omitempty,min=1,max=1024"`
+	Label         string `form:"label" binding:"omitempty,min=1,max=64"`
+	Country       string `form:"country" binding:"omitempty,len=2"`
+	Comment       string `form:"comment" binding:"omitempty,min=1,max=1024"`
+	CatalogNumber string `form:"catalogNumber" binding:"omitempty,min=1,max=16"`
+	LabelCode     int    `form:"labelCode" binding:"omitempty,min=4,max=5"`
 }
