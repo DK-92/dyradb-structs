df6683b630e1a906e4dca6de36192e7e1ea75c52
Niels <avuc.site@gmail.com>
2024-08-30 14:12:39 +0200

    Added XLogIDHeader func to response

--- model/api/response.go
+++ model/api/response.go
@@ -2,6 +2,8 @@ package api
 
 import "net/http"
 
+const xLogIdHeader = "X-LOG-ID"
+
 // Response
 // API responses fit in this struct.
 type Response struct {
@@ -14,4 +16,8 @@ type Response struct {
 // Checks whether the response is valid. Workaround, because the current HTTP client doesn't handle errors very well.
 func (r *Response) IsNotValid() bool {
 	return len(r.Errors) > 0
+}
+
+func (r *Response) XLogIDHeader() string {
+	return r.Headers.Get(xLogIdHeader)
 }
