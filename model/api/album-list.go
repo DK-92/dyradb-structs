a1d6389552fd2d2a59cdab36ffa17bbc1be7012b
Niels Greven <avuc.site@gmail.com>
2026-06-21 13:22:47 +0200

    Encoding and decoding of album list

--- model/api/album-list.go
+++ model/api/album-list.go
@@ -1,5 +1,16 @@
 package api
 
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+)
+
+const (
+	magicPrefix = "DYRADB"
+	magicSuffix = "251225"
+)
+
 // AlbumList
 // Describes a list of albums, together with pagination information.
 type AlbumList struct {
@@ -8,4 +19,58 @@ type AlbumList struct {
 	ExportMeta  *ExportMeta `json:"export_meta,omitempty"`
 
 	Albums []*Album `json:"albums"`
+}
+
+func (al *AlbumList) ExportAsBinary() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(al); err != nil {
+		return nil, err
+	}
+
+	prefix := []byte(magicPrefix)
+	data := buf.Bytes()
+	suffix := []byte(magicSuffix)
+
+	result := join(len(prefix)+len(data)+len(suffix), prefix, data, suffix)
+
+	return result, nil
+}
+
+func ImportToAlbumList(data []byte) (*AlbumList, error) {
+	prefix := []byte(magicPrefix)
+	suffix := []byte(magicSuffix)
+	minLen := len(prefix) + len(suffix)
+
+	if len(data) < minLen {
+		return nil, errors.New("data too short")
+	}
+
+	if !bytes.Equal(data[:len(prefix)], prefix) {
+		return nil, errors.New("invalid prefix")
+	}
+
+	if !bytes.Equal(data[len(data)-len(suffix):], suffix) {
+		return nil, errors.New("invalid suffix")
+	}
+
+	encodedData := data[len(prefix) : len(data)-len(suffix)]
+
+	var al AlbumList
+	dec := gob.NewDecoder(bytes.NewReader(encodedData))
+	if err := dec.Decode(&al); err != nil {
+		return nil, err
+	}
+
+	return &al, nil
+}
+
+func join(s int, ba ...[]byte) []byte {
+	b, i := make([]byte, s), 0
+
+	for _, v := range ba {
+		i += copy(b[i:], v)
+	}
+
+	return b
 }
